0f03d5d3fc055e09be2b023e8bd14454fbfc9fef
Eric Nguyen <53154237+EricNguyen-B@users.noreply.github.com>
2021-06-22 01:31:48 +0000

    Enabled multiple questions and multiple answer inputs

--- candidate-testing.js
+++ candidate-testing.js
@@ -2,15 +2,19 @@ const input = require('readline-sync');
 
 // TODO 2: modify your quiz app to ask 5 questions //
 
+
+
 // TODO 1.1a: Define candidateName // 
 let candidateName = '';
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
+
 let question = 'Who was the first American woman in space?';
 let correctAnswer = 'Sally Ride';
 let candidateAnswer = '';
-let questions = '';
-let correctAnswers = '';
-let candidateAnswers = '';
+
+let questions = ['Who was the first American woman in space?', 'True or false: 5 kilometer === 5000 meters?', '(5+3)/2 * 10 = ?','Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2?', 'What is the minimum crew size for the ISS?'];
+let correctAnswers = ['Sally Ride', 'true', '40', 'Trajectory', '3'];
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -20,20 +24,25 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswer = input.question(question + "\n");
+  // candidateAnswer = input.question(question + "\n");
+  for (let i = 0; i < questions.length; i++) {
+    candidateAnswers[i] = input.question(questions[i] + "\n");
+  }
   
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  if (candidateAnswer == correctAnswer) {
-    console.log("You are correct!");
-  } else {
-    console.log("Your answer is not correct");
-  }
 
   let grade;
+
+  console.log(`Candidate Name: ${candidateName}`)
+  for (let i = 0; i < candidateAnswers.length; i++){
+      console.log(`${i+1}) ${questions[i]}`);
+      console.log(`Your Answer: ${candidateAnswers[i]}`);
+      console.log(`Correct Answer: ${correctAnswers[i]} \n`);
+  }
   
 
   return grade;
