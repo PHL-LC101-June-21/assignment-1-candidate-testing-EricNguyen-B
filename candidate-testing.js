4592b61d31c3f997172d8d102c5f5e1baf947e11
Eric Nguyen <53154237+EricNguyen-B@users.noreply.github.com>
2021-06-28 19:51:53 +0000

    Reformatted single question input with space formatting

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,7 +8,7 @@ const input = require('readline-sync');
 let candidateName = '';
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 
-let question = 'Who was the first American woman in space?';
+let question = 'Who was the first American woman in space? ';
 let correctAnswer = 'Sally Ride';
 let candidateAnswer = '';
 
